6204e062304bced0a66cd76c5fdd7d82c873011a
Raymundo <rramirez5@ucol.mx>
2023-11-29 12:36:53 -0600

    To-Do app
    Proyecto final de la materia aplicaciones móviles

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+// service-worker.js
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('mi-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/app.js',
+          // ...otros archivos
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
